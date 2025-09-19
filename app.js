8718066d0716451a9c90d80e81dfa83eb5f1529f
develperamit001 <developeramit001@gmail.com>
2025-09-19 15:26:14 +0530

    fix env

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ console.log(' ENV_CONTENT', process.env.ENV_CONTENT);
 app.get('/health', (req, res) => {
   res.status(200).json({
     status: 'success',
-    message: `new Backend- is running! ${process.env.TEST} (add new piple lienifdif )`,
+    message: `new Backend- is running! ${process.env.TEST} (add new piple  for env varialbe )`,
     timestamp: new Date(),
   });
 });
